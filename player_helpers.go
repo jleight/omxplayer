e6a1e6179e6d85210872c57646471d33b681f377
Jonathon Leight <jonathon.leight@jleight.com>
2014-11-08 10:59:46 -0500

    Implement all D-Bus helpers.

--- player_helpers.go
+++ player_helpers.go
@@ -1,7 +1,6 @@
 package omxplayer
 
 import (
-	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/guelfey/go.dbus"
 )
@@ -9,35 +8,55 @@ import (
 // dbusCall calls a D-Bus method that has no return value.
 func dbusCall(bus *dbus.Object, path string) error {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
-	return fmt.Errorf("omxplayer: %s not implemented yet", path)
+	return bus.Call(path, 0).Err
 }
 
 // dbusGetBool calls a D-Bus method that will return a boolean value.
 func dbusGetBool(bus *dbus.Object, path string) (bool, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
-	return false, fmt.Errorf("omxplayer: %s not implemented yet", path)
+	call := bus.Call(path, 0)
+	if call.Err != nil {
+		return false, call.Err
+	}
+	return call.Body[0].(bool), nil
 }
 
 // dbusGetFloat64 calls a D-Bus method that will return an int64 value.
 func dbusGetFloat64(bus *dbus.Object, path string) (float64, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
-	return 0, fmt.Errorf("omxplayer: %s not implemented yet", path)
+	call := bus.Call(path, 0)
+	if call.Err != nil {
+		return 0, call.Err
+	}
+	return call.Body[0].(float64), nil
 }
 
 // dbusGetInt64 calls a D-Bus method that will return an int64 value.
 func dbusGetInt64(bus *dbus.Object, path string) (int64, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
-	return 0, fmt.Errorf("omxplayer: %s not implemented yet", path)
+	call := bus.Call(path, 0)
+	if call.Err != nil {
+		return 0, call.Err
+	}
+	return call.Body[0].(int64), nil
 }
 
 // dbusGetString calls a D-Bus method that will return a string value.
 func dbusGetString(bus *dbus.Object, path string) (string, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
-	return "", fmt.Errorf("omxplayer: %s not implemented yet", path)
+	call := bus.Call(path, 0)
+	if call.Err != nil {
+		return "", call.Err
+	}
+	return call.Body[0].(string), nil
 }
 
 // dbusGetStringArray calls a D-Bus method that will return a string array.
 func dbusGetStringArray(bus *dbus.Object, path string) ([]string, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
-	return nil, fmt.Errorf("omxplayer: %s not implemented yet", path)
+	call := bus.Call(path, 0)
+	if call.Err != nil {
+		return nil, call.Err
+	}
+	return call.Body[0].([]string), nil
 }
