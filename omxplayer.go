181f85cfc673bee0d6b7c79aa56d27647d882a93
Jonathon Leight <jonathon.leight@jleight.com>
2014-11-08 09:36:03 -0500

    Add functionality to set user and home directory.
    The latest version of `omxplayer` stores the D-Bus connection path and
    PID in a temp file that has the executing user's name in the filename.
    
    This allows the user of the library to specify what user `omxplayer`
    will be running as in case the `USER` and `HOME` environment variables
    are not available.

---
+++ omxplayer.go
@@ -0,0 +1,28 @@
+package omxplayer
+
+import (
+	"os"
+)
+
+const (
+	prefixOmxDbusFiles = "/tmp/omxplayerdbus."
+	suffixOmxDbusPid   = ".pid"
+)
+
+var (
+	user            string
+	home            string
+	fileOmxDbusPath string
+	fileOmxDbusPid  string
+)
+
+func init() {
+	SetUser(os.Getenv("USER"), os.Getenv("HOME"))
+}
+
+func SetUser(u, h string) {
+	user = u
+	home = h
+	fileOmxDbusPath = prefixOmxDbusFiles + user
+	fileOmxDbusPid = prefixOmxDbusFiles + user + suffixOmxDbusPid
+}
