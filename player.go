03d6bd16b7aa54a1ab8f68ca0048eebaa4b78ea3
Jonathon Leight <jonathon.leight@jleight.com>
2014-11-08 10:55:55 -0500

    Change all D-Bus stub methods to accept a bus.

--- player.go
+++ player.go
@@ -2,6 +2,7 @@ package omxplayer
 
 import (
 	"fmt"
+	"github.com/guelfey/go.dbus"
 )
 
 const (
@@ -56,86 +57,87 @@ const (
 )
 
 type Player struct {
+	bus *dbus.Object
 }
 
 func (p *Player) Quit() error {
-	return dbusCall(cmdQuit)
+	return dbusCall(p.bus, cmdQuit)
 }
 
 func (p *Player) CanQuit() (bool, error) {
-	return dbusGetBool(propCanQuit)
+	return dbusGetBool(p.bus, propCanQuit)
 }
 
 func (p *Player) Fullscreen() (bool, error) {
-	return dbusGetBool(propFullscreen)
+	return dbusGetBool(p.bus, propFullscreen)
 }
 
 func (p *Player) CanSetFullscreen() (bool, error) {
-	return dbusGetBool(propCanSetFullscreen)
+	return dbusGetBool(p.bus, propCanSetFullscreen)
 }
 
 func (p *Player) CanRaise() (bool, error) {
-	return dbusGetBool(propCanRaise)
+	return dbusGetBool(p.bus, propCanRaise)
 }
 
 func (p *Player) HasTrackList() (bool, error) {
-	return dbusGetBool(propHasTrackList)
+	return dbusGetBool(p.bus, propHasTrackList)
 }
 
 func (p *Player) Identity() (string, error) {
-	return dbusGetString(propIdentity)
+	return dbusGetString(p.bus, propIdentity)
 }
 
 func (p *Player) SupportedUriSchemes() ([]string, error) {
-	return dbusGetStringArray(propSupportedUriSchemes)
+	return dbusGetStringArray(p.bus, propSupportedUriSchemes)
 }
 
 func (p *Player) SupportedMimeTypes() ([]string, error) {
-	return dbusGetStringArray(propSupportedMimeTypes)
+	return dbusGetStringArray(p.bus, propSupportedMimeTypes)
 }
 
 func (p *Player) CanGoNext() (bool, error) {
-	return dbusGetBool(propCanGoNext)
+	return dbusGetBool(p.bus, propCanGoNext)
 }
 
 func (p *Player) CanGoPrevious() (bool, error) {
-	return dbusGetBool(propCanGoPrevious)
+	return dbusGetBool(p.bus, propCanGoPrevious)
 }
 
 func (p *Player) CanSeek() (bool, error) {
-	return dbusGetBool(cmdSeek)
+	return dbusGetBool(p.bus, cmdSeek)
 }
 
 func (p *Player) CanControl() (bool, error) {
-	return dbusGetBool(propCanControl)
+	return dbusGetBool(p.bus, propCanControl)
 }
 
 func (p *Player) CanPlay() (bool, error) {
-	return dbusGetBool(propCanPlay)
+	return dbusGetBool(p.bus, propCanPlay)
 }
 
 func (p *Player) CanPause() (bool, error) {
-	return dbusGetBool(propCanPause)
+	return dbusGetBool(p.bus, propCanPause)
 }
 
 func (p *Player) Next() error {
-	return dbusCall(cmdNext)
+	return dbusCall(p.bus, cmdNext)
 }
 
 func (p *Player) Previous() error {
-	return dbusCall(cmdPrevious)
+	return dbusCall(p.bus, cmdPrevious)
 }
 
 func (p *Player) Pause() error {
-	return dbusCall(cmdPause)
+	return dbusCall(p.bus, cmdPause)
 }
 
 func (p *Player) PlayPause() error {
-	return dbusCall(cmdPlayPause)
+	return dbusCall(p.bus, cmdPlayPause)
 }
 
 func (p *Player) Stop() error {
-	return dbusCall(cmdStop)
+	return dbusCall(p.bus, cmdStop)
 }
 
 func (p *Player) Seek(amount int64) (int64, error) {
@@ -147,7 +149,7 @@ func (p *Player) SetPosition(path string, position int64) (int64, error) {
 }
 
 func (p *Player) PlaybackStatus() (string, error) {
-	return dbusGetString(propPlaybackStatus)
+	return dbusGetString(p.bus, propPlaybackStatus)
 }
 
 func (p *Player) Volume(volume ...float64) (float64, error) {
@@ -155,63 +157,63 @@ func (p *Player) Volume(volume ...float64) (float64, error) {
 }
 
 func (p *Player) Mute() error {
-	return dbusCall(cmdMute)
+	return dbusCall(p.bus, cmdMute)
 }
 
 func (p *Player) Unmute() error {
-	return dbusCall(cmdUnmute)
+	return dbusCall(p.bus, cmdUnmute)
 }
 
 func (p *Player) Position() (int64, error) {
-	return dbusGetInt64(propPosition)
+	return dbusGetInt64(p.bus, propPosition)
 }
 
 func (p *Player) Aspect() (float64, error) {
-	return dbusGetFloat64(propAspect)
+	return dbusGetFloat64(p.bus, propAspect)
 }
 
 func (p *Player) VideoStreamCount() (int64, error) {
-	return dbusGetInt64(propVideoStreamCount)
+	return dbusGetInt64(p.bus, propVideoStreamCount)
 }
 
 func (p *Player) ResWidth() (int64, error) {
-	return dbusGetInt64(propResWidth)
+	return dbusGetInt64(p.bus, propResWidth)
 }
 
 func (p *Player) ResHeight() (int64, error) {
-	return dbusGetInt64(propResHeight)
+	return dbusGetInt64(p.bus, propResHeight)
 }
 
 func (p *Player) Duration() (int64, error) {
-	return dbusGetInt64(propDuration)
+	return dbusGetInt64(p.bus, propDuration)
 }
 
 func (p *Player) MinimumRate() (float64, error) {
-	return dbusGetFloat64(propMinimumRate)
+	return dbusGetFloat64(p.bus, propMinimumRate)
 }
 
 func (p *Player) MaximumRate() (float64, error) {
-	return dbusGetFloat64(propMaximumRate)
+	return dbusGetFloat64(p.bus, propMaximumRate)
 }
 
 func (p *Player) ListSubtitles() ([]string, error) {
-	return dbusGetStringArray(cmdListSubtitles)
+	return dbusGetStringArray(p.bus, cmdListSubtitles)
 }
 
 func (p *Player) HideVideo() error {
-	return dbusCall(cmdHideVideo)
+	return dbusCall(p.bus, cmdHideVideo)
 }
 
 func (p *Player) UnHideVideo() error {
-	return dbusCall(cmdUnHideVideo)
+	return dbusCall(p.bus, cmdUnHideVideo)
 }
 
 func (p *Player) ListAudio() ([]string, error) {
-	return dbusGetStringArray(cmdListAudio)
+	return dbusGetStringArray(p.bus, cmdListAudio)
 }
 
 func (p *Player) ListVideo() ([]string, error) {
-	return dbusGetStringArray(cmdListVideo)
+	return dbusGetStringArray(p.bus, cmdListVideo)
 }
 
 func (p *Player) SelectSubtitle(index int32) (bool, error) {
@@ -223,11 +225,11 @@ func (p *Player) SelectAudio(index int32) (bool, error) {
 }
 
 func (p *Player) ShowSubtitles() error {
-	return dbusCall(cmdShowSubtitles)
+	return dbusCall(p.bus, cmdShowSubtitles)
 }
 
 func (p *Player) HideSubtitles() error {
-	return dbusCall(cmdHideSubtitles)
+	return dbusCall(p.bus, cmdHideSubtitles)
 }
 
 func (p *Player) Action(action int32) error {
--- player_helpers.go
+++ player_helpers.go
@@ -3,40 +3,41 @@ package omxplayer
 import (
 	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"github.com/guelfey/go.dbus"
 )
 
 // dbusCall calls a D-Bus method that has no return value.
-func dbusCall(path string) error {
+func dbusCall(bus *dbus.Object, path string) error {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
 	return fmt.Errorf("omxplayer: %s not implemented yet", path)
 }
 
 // dbusGetBool calls a D-Bus method that will return a boolean value.
-func dbusGetBool(path string) (bool, error) {
+func dbusGetBool(bus *dbus.Object, path string) (bool, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
 	return false, fmt.Errorf("omxplayer: %s not implemented yet", path)
 }
 
 // dbusGetFloat64 calls a D-Bus method that will return an int64 value.
-func dbusGetFloat64(path string) (float64, error) {
+func dbusGetFloat64(bus *dbus.Object, path string) (float64, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
 	return 0, fmt.Errorf("omxplayer: %s not implemented yet", path)
 }
 
 // dbusGetInt64 calls a D-Bus method that will return an int64 value.
-func dbusGetInt64(path string) (int64, error) {
+func dbusGetInt64(bus *dbus.Object, path string) (int64, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
 	return 0, fmt.Errorf("omxplayer: %s not implemented yet", path)
 }
 
 // dbusGetString calls a D-Bus method that will return a string value.
-func dbusGetString(path string) (string, error) {
+func dbusGetString(bus *dbus.Object, path string) (string, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
 	return "", fmt.Errorf("omxplayer: %s not implemented yet", path)
 }
 
 // dbusGetStringArray calls a D-Bus method that will return a string array.
-func dbusGetStringArray(path string) ([]string, error) {
+func dbusGetStringArray(bus *dbus.Object, path string) ([]string, error) {
 	log.WithFields(log.Fields{"path": path}).Debug("omxplayer: dbus call")
 	return nil, fmt.Errorf("omxplayer: %s not implemented yet", path)
 }
