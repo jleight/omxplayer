ce68e885ef785247a1b0e418184b13c95fcb9037
Jonathon Leight <jonathon.leight@jleight.com>
2014-11-09 10:15:36 -0500

    Implement functions for getting D-Bus information.

--- omxplayer.go
+++ omxplayer.go
@@ -42,3 +42,23 @@ func removeDbusFiles() {
 	removeFile(fileOmxDbusPath)
 	removeFile(fileOmxDbusPid)
 }
+
+// getDbusPath reads the D-Bus path from the file OMXPlayer writes it's path to.
+// If the file cannot be read, it returns an error, otherwise it returns the
+// path as a string.
+func getDbusPath() (string, error) {
+	if err := waitForFile(fileOmxDbusPath); err != nil {
+		return "", err
+	}
+	return readFile(fileOmxDbusPath)
+}
+
+// getDbusPath reads the D-Bus PID from the file OMXPlayer writes it's PID to.
+// If the file cannot be read, it returns an error, otherwise it returns the
+// PID as a string.
+func getDbusPid() (string, error) {
+	if err := waitForFile(fileOmxDbusPid); err != nil {
+		return "", err
+	}
+	return readFile(fileOmxDbusPid)
+}
