7aed7c9d88e9110b8f263c88b482b3c46310f344
Jonathon Leight <jonathon.leight@jleight.com>
2014-11-09 09:36:55 -0500

    Implement a waitForFile function.
    Since `omxplayer` does not create its D-Bus files as soon as it opens,
    the library needs to wait until the files appear before it can attempt
    to connect to D-Bus.

--- omxplayer_helpers.go
+++ omxplayer_helpers.go
@@ -1,8 +1,10 @@
 package omxplayer
 
 import (
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"os"
+	"time"
 )
 
 // removeFile removes the specified file. Errors are ignored.
@@ -11,4 +13,21 @@ func removeFile(path string) {
 		"path": path,
 	}).Debug("omxplayer: removing file")
 	os.Remove(path)
+}
+
+// waitForFile waits for the specified file to exist before returning. If the an
+// error, other than the file not existing, occurs, the error is returned. If,
+// after 100 attempts, the file does not exist, an error is returned.
+func waitForFile(path string) error {
+	log.WithFields(log.Fields{
+		"file": path,
+	}).Debug("omxplayer: waiting for file")
+	for i := 0; i < 100; i++ {
+		_, err := os.Stat(path)
+		if err == nil || !os.IsNotExist(err) {
+			return err
+		}
+		time.Sleep(50 * time.Millisecond)
+	}
+	return fmt.Errorf("omxplayer: file does not exist: %s", path)
 }
